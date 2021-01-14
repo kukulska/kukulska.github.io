e7fee5fda86210f78a2a06c643446992bcbf15e7
Agata Kukulska <a.kukulska@pm.me>
2021-01-14 12:23:07 +0000

    Changed image behavior, now they are in color when the section is in the middle of the viewport

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//images in color and BW
 const images = {
   tokyo: {
     bw: "./img/Tokyo_BW.jpg",
@@ -13,12 +14,34 @@ const images = {
   },
 };
 
-function hover(element) {
-  let name = element.getAttribute("id");
-  element.setAttribute("src", images[name].color);
+//images change color when they are in the middle of the Viewport
+
+const sections = document.querySelectorAll("section.project");
+
+function changeImageColor() {
+  const topViewport = window.pageYOffset;
+  const midViewport = topViewport + window.innerHeight / 2;
+
+  sections.forEach((section) => {
+    const topSection = section.offsetTop;
+    const midSection = topSection + section.offsetHeight / 2;
+
+    const distanceToSection = Math.abs(midViewport - midSection);
+
+    const image = section.querySelector("img");
+
+    if (distanceToSection < section.offsetHeight / 2) {
+      let name = image.getAttribute("id");
+      image.setAttribute("src", images[name].color);
+    } else {
+      let name = image.getAttribute("id");
+      image.setAttribute("src", images[name].bw);
+    }
+  });
 }
 
-function unhover(element) {
-  let name = element.getAttribute("id");
-  element.setAttribute("src", images[name].bw);
-}
+document.addEventListener("scroll", function () {
+  changeImageColor();
+});
+
+changeImageColor();
