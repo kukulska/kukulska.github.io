c4b9ef1e989bf6dff17202eade736d475e8324b7
Agata Kukulska <a.kukulska@pm.me>
2021-02-05 12:42:17 +0000

    Added fade in effect on header and projects texts

--- script.js
+++ script.js
@@ -43,3 +43,39 @@ document.addEventListener("scroll", function () {
 
   progressTag.style.width = `${100 * percentage}%`;
 });
+
+// Projects descriptions fading in
+
+const animatedTags = document.querySelectorAll(
+  "section.project h3, section.project p, div.landing h1, div.landing h2, div.landing ul"
+);
+
+animatedTags.forEach((tag) => {
+  tag.style.opacity = 0;
+});
+
+function fadeIn() {
+  let delay = 0.25;
+
+  animatedTags.forEach((tag) => {
+    const tagTop = tag.getBoundingClientRect().top;
+    const tagBottom = tag.getBoundingClientRect().bottom;
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadein 1s ${delay}s both`;
+      delay = delay + 0.25;
+    } else {
+      tag.style.opacity = 0;
+      tag.style.animation = "";
+    }
+  });
+}
+
+document.addEventListener("scroll", function () {
+  fadeIn();
+});
+
+window.addEventListener("resize", function () {
+  fadeIn();
+});
+
+fadeIn();
